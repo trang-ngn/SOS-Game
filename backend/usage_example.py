6602e6d5546cbb6530297692dccd4a158c531d57
Trang Nguyen <thuy-trang.nguyen@tu-braunschweig.de>
2025-04-30 23:15:01 +0200

    feat: add an example of using the solver

---
+++ backend/usage_example.py
@@ -0,0 +1,22 @@
+from rescue_stations import (
+    RescueStationInstance,
+    RescueStationSolver,
+    RescueStationSolution,
+)
+
+instance = RescueStationInstance(
+    n=5,  # 5 houses
+    costs=[3.0, 3.5, 7.0, 3.5],  # cost of each station
+    coverage=[  # list of houses that each station covers, every house must be covered at least by one station
+        [0, 1],
+        [1, 2, 3],
+        [3, 4],
+        [0, 2, 4],
+    ],
+)
+
+solver = RescueStationSolver(instance=instance, verbose=False)
+solution: RescueStationSolution = solver.solve()
+selected_stations: list[bool] = solution.selected
+
+print(solution)
