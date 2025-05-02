6b72b9e5104df4d77489544793db32dd7208a852
Fangjie Lian <f.lian@tu-braunschweig.de>
2025-05-02 16:41:19 +0200

    scene_start

---
+++ sos-game/Scripts/button_sandbox.gd
@@ -0,0 +1,12 @@
+extends TextureButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	position = Vector2(660, 562) 
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
