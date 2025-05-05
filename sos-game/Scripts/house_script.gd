189c69f5bc62660af34cc00575586bc8a835e0f5
Fadhil <m.atsari@tu-braunschweig.de>
2025-05-05 11:46:10 +0200

    fix : fix bug where station overwrite the value of house that covered by multiple station

--- sos-game/Scripts/house_script.gd
+++ sos-game/Scripts/house_script.gd
@@ -1,15 +1,24 @@
 class_name House
 extends StaticBody2D
 
-var covered : bool = false 
+var num_stat_cover : int = 0 
 @export var id : int 
 
 func is_covered() -> bool :
-	return covered
+	if(num_stat_cover > 0) :
+		return true
+	else :
+		return false
+
+#func _process(delta: float) -> void:
+	#print(num_stat_cover)
 
 func _ready() -> void:
 	$HouseNumber.visible = false
-	set_outline(false)
+	set_outline()
 	
-func set_outline(value : bool) -> void :
-	$Sprite2D.set_instance_shader_parameter("active",value)
+func set_outline() -> void :
+	if(num_stat_cover == 0) :
+		$Sprite2D.set_instance_shader_parameter("active",false)
+	else :
+		$Sprite2D.set_instance_shader_parameter("active",true)
