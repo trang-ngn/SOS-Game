0e35b56eebfe30bdc7e2ba0a67e5380b9f86eeed
Fangjie Lian <f.lian@tu-braunschweig.de>
2025-05-19 00:26:55 +0200

    feat: back-to-menu Confirm Dialog #18

--- sos-game/Scripts/level_script.gd
+++ sos-game/Scripts/level_script.gd
@@ -94,6 +94,7 @@ func _on_hide_button_toggled(is_hidden: bool) -> void:
 func _on_restart_button_pressed() -> void:
 	get_tree().reload_current_scene()
 
+
 func _on_back_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/start.tscn")
-	pass
+	print("Back button pressed!")
+	$Confirmdialog_BackToStart.visible = true
