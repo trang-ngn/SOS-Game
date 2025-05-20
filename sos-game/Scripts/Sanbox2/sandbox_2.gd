5ade6b75410c6f5fc13427d2661890697b8cc529
Fadhil <m.atsari@tu-braunschweig.de>
2025-05-20 19:18:27 +0200

    fix : fix overlapping mode between build and delete

--- sos-game/Scripts/Sanbox2/sandbox_2.gd
+++ sos-game/Scripts/Sanbox2/sandbox_2.gd
@@ -2,22 +2,29 @@ extends Node2D
 
 @onready var house_scene = preload("res://Scenes/Objects/house_sandbox.tscn")
 @onready var houses = $Houses
-@onready var default_object : ObjectSandbox = $PreviewObjects/DefaultObject
-@onready var object_deleter : ObjectSandbox = $PreviewObjects/ObjectDeleter
+@onready var default_object : DefaultObject = $PreviewObjects/DefaultObject
+@onready var object_deleter : ObjectsDeleter = $PreviewObjects/ObjectDeleter
 @onready var tilemap : TileMap = $TileMap
 
 var current_grid_position
 var current_object : ObjectSandbox
-var is_building = false
-var is_deleting = false
-var object_to_delete : ObjectSandbox = null
+var is_building 
+var is_deleting 
+
+enum MODE{
+	DEFAULT,
+	BUILD,
+	DELETE
+}
 
 func _ready() -> void:
-	current_object = default_object
+	switch_mode(MODE.DEFAULT)
+	
 	
 func _process(delta: float) -> void:
 	check_area()
-	print(houses.get_child_count())
+	#debug purpose
+	#print(houses.get_child_count())
 
 func check_area() -> void :
 	var mouse_tile = get_global_mouse_position()
@@ -28,25 +35,59 @@ func check_area() -> void :
 	
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_released():
-		is_deleting = false
-		if !is_building :
-			change_to_object()
-			is_building = true
-		else :
+		if !is_building and !is_deleting :
+			switch_mode(MODE.BUILD)
+			change_to_house()
+		
+		elif is_building :
 			place_object()
-
+		
+		elif is_deleting :
+			delete_object()
+		
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT and event.is_released():
-		is_building = false
 		if !is_deleting :
-			current_object = object_deleter
-			is_deleting = true
-		elif object_to_delete == null :
-			pass
-		else :
-			object_to_delete.queue_free()
-			
-func change_to_object() -> void :
-	default_object.visible = false
+			switch_mode(MODE.DELETE)
+		elif is_deleting :
+			switch_mode(MODE.DEFAULT)
+	
+
+#this function can be splited in to more function but for now this works!
+func switch_mode(mode : MODE ) -> void :
+	if mode == MODE.DEFAULT :
+		default_mode()
+	elif mode == MODE.BUILD :
+		build_mode()
+	elif  mode == MODE.DELETE :
+		delete_mode()
+	else :
+		print("HOW DO WE GET HERE????????")
+
+#these three functions set the parameters according to the mode
+#why don't i just call this function to switch mode? idk i just realized that
+#switch_mode sound cooler though
+func default_mode() -> void :
+	default_object.switch_default(true)
+	object_deleter.switch_deleter(false)
+	current_object = default_object
+	is_building = false
+	is_deleting = false
+
+func build_mode() -> void :
+	default_object.switch_default(false)
+	object_deleter.switch_deleter(false)
+	#current_object will be changed in other function according to the building type
+	is_building = true
+	is_deleting = false
+
+func delete_mode() -> void :
+	default_object.switch_default(false)
+	object_deleter.switch_deleter(true)
+	current_object = object_deleter
+	is_building = false
+	is_deleting = true
+
+func change_to_house() -> void :
 	var new_house = house_scene.instantiate()
 	houses.add_child(new_house)
 	current_object = new_house
@@ -55,16 +96,11 @@ func place_object() -> void :
 	if current_object.is_colliding() :
 		print("This object is overlapping with another object")
 	else :
-		current_object.position = current_grid_position
-		current_object = default_object
-		current_object.visible = true
-		is_building = false
+		current_object.position = current_grid_position 
+		switch_mode(MODE.DEFAULT)
 
-func _on_object_deleter_area_entered(area: HouseSandbox) -> void:
-	if area is HouseSandbox :
-		object_to_delete = area
-	print(object_to_delete)
-
-func _on_object_deleter_area_exited(area: HouseSandbox) -> void:
-	object_to_delete = null
-	print(object_to_delete)
+func delete_object() -> void :
+	if object_deleter.collided_object == null :
+		print("hover over to an object")
+	else :
+		object_deleter.collided_object.queue_free()
