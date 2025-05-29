300a2d2377b561999b76c73280ebfc5ff0d54d58
Trang Nguyen <thuy-trang.nguyen@tu-braunschweig.de>
2025-05-29 14:47:16 +0200

    fix: minor

--- sos-game/Scenes/Levels/level_picker.gd
+++ sos-game/Scenes/Levels/level_picker.gd
@@ -6,12 +6,12 @@ func _on_back_button_pressed() -> void:
 
 
 func _on_button_level_1_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Levels/level_1_auto.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Levels/level_1.tscn")
 	
 
 func _on_button_level_2_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Levels/level_2_auto.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Levels/level_2.tscn")
 
 
 func _on_button_level_3_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Levels/level_3_auto.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Levels/level_3.tscn")
