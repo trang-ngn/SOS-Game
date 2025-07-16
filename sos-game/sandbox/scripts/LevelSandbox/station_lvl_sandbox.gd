1122f7538ff7c78092103784789f85f6b1a40ce1
Fadhil <m.atsari@tu-braunschweig.de>
2025-07-16 18:07:57 +0200

    minor fix

--- sos-game/sandbox/scripts/LevelSandbox/station_lvl_sandbox.gd
+++ sos-game/sandbox/scripts/LevelSandbox/station_lvl_sandbox.gd
@@ -94,8 +94,8 @@ func cover_houses(value: bool)->void:
 func play_animation()-> void:
 
 	designs[design_index].set_instance_shader_parameter("wiggle_strength", 2.4)
-	var total_time:float = 0.25
-	var interval: float = 0.05
+	var total_time:float = 1
+	var interval: float = 0.1
 	var elapsed: float = 0.0
 	var new_progress: float = 0.0
 
